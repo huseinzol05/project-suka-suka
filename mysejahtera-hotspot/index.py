bdc0a22933538da500e292c2723662be9cd637af
husein zolkepli <husein.zol05@gmail.com>
2021-07-11 14:28:00 +0800

    added script without convex

--- mysejahtera-hotspot/index.py
+++ mysejahtera-hotspot/index.py
@@ -1,3 +1,4 @@
+import json
 from flask import Flask
 from flask import render_template
 
@@ -7,6 +8,18 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return render_template('./index.html')
+
+
+@app.route("/test")
+def test():
+    return render_template('./selangor.html')
+
+
+@app.route("/selangor")
+def selangor():
+    with open('data/selangor-test.json') as fopen:
+        data = json.load(fopen)
+    return json.dumps(data)
 
 
 if __name__ == "__main__":
